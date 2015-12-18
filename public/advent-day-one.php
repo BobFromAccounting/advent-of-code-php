81804bcddceaa0c3af33f0da3ab6ec6fa24d7f5a
Tarek S Hafez <thafez@ymail.com>
2015-12-18 11:45:39 -0600

    day one completed php parser.

---
+++ public/advent-day-one.php
@@ -0,0 +1,36 @@
+<?php
+
+    $filename = "advent-day-one-input.txt";
+
+    $handle = fopen($filename, "r");
+
+    $contents = fread($handle, filesize($filename));
+
+    fclose($handle);
+
+    $length = strlen($contents);
+
+    $inputs = [];
+
+    $endingFloor = 0;
+
+    for($i = 0; $i < $length; $i += 1) {
+        $inputs[$i] = $contents[$i];
+    }
+
+    foreach ($inputs as $key => $input) {
+        if ($endingFloor === -1) {
+            echo $key . PHP_EOL;
+            die();
+        }
+
+        if ($input === "(") {
+           $endingFloor += 1; 
+        } else if ($input === ")") {
+            $endingFloor -= 1;
+        }
+    }
+
+    echo $endingFloor . PHP_EOL;
+
+?>
